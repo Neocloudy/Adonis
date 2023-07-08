93cc1c303cbf588c0f3582abe1609b19465aae0d
Neocloudy <88008002+Neocloudy@users.noreply.github.com>
2023-07-07 23:23:30 -0500

    port age restrictions

--- MainModule/Server/Dependencies/DefaultSettings.lua
+++ MainModule/Server/Dependencies/DefaultSettings.lua
@@ -308,6 +308,7 @@ settings.Allowed_API_Calls = {
 
 settings.FunCommands = true				-- Are fun commands enabled?
 settings.PlayerCommands = true			-- Are player-level utility commands enabled?
+settings.AgeRestrictedCommands = true	-- Are age-restricted commands enabled?
 settings.CommandFeedback = false		-- Should players be notified when commands with non-obvious effects are run on them?
 settings.CrossServerCommands = true		-- Are commands which affect more than one server enabled?
 settings.ChatCommands = true			-- If false you will not be able to run commands via the chat; Instead, you MUST use the console or you will be unable to run commands
@@ -426,6 +427,7 @@ descs.Allowed_API_Calls = [[ Allowed calls ]]
 
 descs.FunCommands = [[ Are fun commands enabled? ]]
 descs.PlayerCommands = [[ Are players commands enabled? ]]
+descs.AgeRestrictedCommands = [[ Are age-restricted commands enabled? ]]
 descs.CommandFeedback = [[ Should players be notified when commands with non-obvious effects are run on them? ]]
 descs.CrossServerCommands = [[ Are commands which affect more than one server enabled? ]]
 descs.ChatCommands = [[ If false you will not be able to run commands via the chat; Instead, you MUST use the console or you will be unable to run commands ]]
